4ecc8e862e1bb31b00cc7370aee2ceac2d23250a
Nicolas Romero <nicolasdev290300@gmail.com>
2024-09-16 20:42:07 -0300

    agregando nuevas funciones

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const app = express()
 const httpServer = app.listen(8080, () => {
     console.log('Escuchando en puerto 8080')
 })
- 
 const io = new Server(httpServer)
 
 app.engine('handlebars', handlebars.engine())
@@ -25,16 +24,102 @@ app.use('/', viewsRouter)
 
 let messages = []
 
+let users = [] 
+
 io.on('connection', socket => {
     console.log("Nuevo cliente conectado")
+    
     socket.on('message', (data) => {
         messages.push(data)
         io.emit('messageLogs', messages)
     })
 
-    socket.on('userAuthenticated', user => {
-        socket.emit('messageLogs', messages)
+    socket.on('usuarioNuevoConectado', user => {
+        users[socket.id] = user
+        io.emit('userList', Object.values(users))
+
+        socket.emit('historialDelChat', messages)
 
         socket.broadcast.emit('newUserConnected', user)
     })
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('usuarioEscribiendo', users[socket.id])
+    })
+
+    socket.on('disconnect', () => {
+        delete users[socket.id]
+        io.emit('userList', Object.values(users))
+    })
+
+    // otras funcionalidades
+    // notificaciones de usuario desconectado
+    socket.on('disconnect', () => {
+        let user = users[socket.id];
+        delete users[socket.id];
+        io.emit('userList', Object.values(users));
+        if (user) {
+            io.emit('userDisconnected', user);
+        }
+    });
+    
+    // mensajes privados
+    socket.on('privateMessage', ({ toUserId, message }) => {
+        socket.to(toUserId).emit('privateMessage', {
+            fromUser: users[socket.id],
+            message: message
+        });
+    });
+
+    // sala de chats
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        socket.emit('message', { user: 'Sistema', message: `Te has unido a la sala: ${room}` });
+    });
+    
+    socket.on('roomMessage', ({ room, message }) => {
+        io.to(room).emit('message', { user: users[socket.id], message: message });
+    });
+    
+    // notificaciones de errores
+    socket.on('message', (data) => {
+        if (!data.message || data.message.trim() === "") {
+            socket.emit('errorMessage', 'El mensaje no puede estar vacío.');
+        } else {
+            messages.push(data);
+            io.emit('messageLogs', messages);
+        }
+    });
+    
+    // sistema de reacciones (emojis)
+    socket.on('reactToMessage', ({ messageId, reaction }) => {
+        let message = messages.find(msg => msg.id === messageId);
+        if (message) {
+            message.reactions = message.reactions || [];
+            message.reactions.push(reaction);
+            io.emit('messageReaction', { messageId, reaction });
+        }
+    });
+    
+    // // compartir archivos
+    // socket.on('sendFile', (file) => {
+    //     io.emit('receiveFile', { user: users[socket.id], file: file });
+    // });
+    
+    // mostrar estado de conexion
+    io.on('connection', socket => {
+        users[socket.id] = { user: "Anonimo" };
+        io.emit('userConnected', users[socket.id]);
+        socket.on('disconnect', () => {
+            io.emit('userDisconnected', users[socket.id]);
+            delete users[socket.id];
+        });
+    });
+    
+    // compartir ubicacion
+    socket.on('shareLocation', (location) => {
+        io.emit('receiveLocation', { user: users[socket.id], location: location });
+    });
+    
+    
 })
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -15,7 +15,7 @@ Swal.fire({
 }).then(result => {
     user = result.value
     document.getElementById('username').textContent = user
-    socket.emit('userAuthenticated', {user: user})
+    socket.emit('usuarioNuevoConectado', { user: user })
 })
 
 chatBox.addEventListener('keyup', (evt) => {
@@ -27,6 +27,10 @@ chatBox.addEventListener('keyup', (evt) => {
     }
 })
 
+chatBox.addEventListener('input', () => {
+    socket.emit('typing')
+})
+
 socket.on('messageLogs', (data) => {
     let log = document.getElementById('messageLogs')
     let messagesHtml = ""
@@ -34,6 +38,15 @@ socket.on('messageLogs', (data) => {
         messagesHtml += `${message.user} dice: ${message.message}<br>`
     })
     log.innerHTML = messagesHtml
+})
+
+socket.on('userList', (data) => {
+    let userList = document.getElementById('userList')
+    let userHtml = ""
+    data.forEach(user => {
+        userHtml += `${user.user} <br>`
+    })
+    userList.innerHTML = userHtml
 })
 
 socket.on('newUserConnected', newUser => {
@@ -47,3 +60,104 @@ socket.on('newUserConnected', newUser => {
         timer: 5000
     })
 })
+
+socket.on('usuarioEscribiendo', (user) => {
+    let objectTyping = document.getElementById('typing')
+    objectTyping.innerHTML = user.user + ' esta escribiendo...'
+
+})
+
+// otras funciones
+// notificaciones de usuario desconectado
+socket.on('userDisconnected', (user) => {
+    Swal.fire({
+        text: `${user} se ha desconectado.`,
+        toast: true,
+        position: 'top-right',
+        icon: 'warning',
+        showConfirmButton: false,
+        timer: 3000
+    });
+});
+
+// enviar msj privado
+
+function sendPrivateMessage(toUserId, message) {
+    socket.emit('privateMessage', { toUserId: toUserId, message: message });
+}
+// recibir msj privados
+socket.on('privateMessage', (data) => {
+    Swal.fire({
+        text: `Mensaje privado de ${data.fromUser}: ${data.message}`,
+        toast: true,
+        position: 'top-right',
+        icon: 'info',
+        showConfirmButton: true
+    });
+});
+
+// unirse a una sala
+socket.emit('joinRoom', 'Sala1');
+// enviar msj a una sala
+function sendRoomMessage(room, message) {
+    socket.emit('roomMessage', { room: room, message: message });
+}
+
+// notificacion de errores
+socket.on('errorMessage', (error) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: error,
+    });
+});
+
+// enviar reaccion
+function reactToMessage(messageId, reaction) {
+    socket.emit('reactToMessage', { messageId: messageId, reaction: reaction });
+}
+// recibir reaccion
+socket.on('messageReaction', ({ messageId, reaction }) => {
+    // Mostrar la reacción al mensaje correspondiente
+    let messageElement = document.querySelector(`[data-id="${messageId}"]`);
+    if (messageElement) {
+        messageElement.innerHTML += ` <span>${reaction}</span>`;
+    }
+});
+
+// // enviar archivo
+// document.getElementById('fileInput').addEventListener('change', function () {
+//     let file = this.files[0];
+//     let reader = new FileReader();
+//     reader.onload = function (e) {
+//         socket.emit('sendFile', e.target.result);
+//     };
+//     reader.readAsDataURL(file);
+// });
+// // recibir archivo
+// socket.on('receiveFile', (data) => {
+//     let log = document.getElementById('messageLogs');
+//     log.innerHTML += `${data.user} ha enviado un archivo: <a href="${data.file}" download="file">Descargar</a><br>`;
+// });
+
+socket.on('userConnected', (user) => {
+    document.getElementById('status').innerHTML = `${user.user} se ha conectado`;
+});
+
+socket.on('userDisconnected', (user) => {
+    document.getElementById('status').innerHTML = `${user.user} se ha desconectado`;
+});
+
+// compartir ubicacion
+navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('shareLocation', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    });
+});
+// recibir ubicacion
+
+
+
+
+
